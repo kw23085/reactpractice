f27d79930c0a1de389166d15cc4738be416f3f70
Kevin Wang <kw1017@gmail.com>
2021-07-26 20:04:00 -0700

    practice

---
+++ src/App2.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+
+function App2() {
+ 
+    return(
+
+        <React.Fragment>
+            <p>test</p>
+            <video src="http://www.w3schools.com/html/mov_bbb.mp4"></video>
+        </React.Fragment>
+
+    )
+}
+
+function sideMenu() {
+
+
+    return(
+
+        <React.Fragment>
+            <div className="side-menu"></div>
+        </React.Fragment>
+
+    )
+
+}
+
+
+export default App2
