a2b6fdc1acb8b9511a8e12951f5844881af59dd3
Kevin Wang <kw1017@gmail.com>
2021-07-29 22:58:22 -0700

    api

--- src/App3.js
+++ src/App3.js
@@ -1,68 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+let BASE_URL = 'https://jsonplaceholder.typicode.com/todos'
+// let BASE_URL = 'https://jsonplaceholder.typicode.com/todos/2'
 
 function App3() {
 
-    let competitions = [
-        ['HTML', 'c#'],
-        ['c#', 'Python'],
-        ['Python', 'HTML']
-    ]
+    const [todos, setTodos] = useState([])
 
-    let results = [0, 0, 1]
-
-    function tourneyWinner(competitions, results) {
-
-        let HOME_TEAM_WON = 1
-
-        let currentBestTeam = ''
-
-        let scores = {[currentBestTeam]: 0}
-
-        for(let i = 0; i < results.length; i++) {
-            
-            let result = results[i]
-
-            let [homeTeam, awayTeam] = competitions[i]
-
-            let winningTeam = result === HOME_TEAM_WON ? homeTeam : awayTeam
-
-            updateScore(scores, winningTeam, 3)
-
-            if(scores[winningTeam] > scores[currentBestTeam]) {
-                currentBestTeam = winningTeam
-            }
-
-        }
-
-        return currentBestTeam
-        
-        function updateScore(scores, team, points) {
-
-            if(!(team in scores)) {
-                scores[team] = 0
-            }
-
-            scores[team] += points
-
-
-        }
-
-
+    function fetchTodos(num) {
+        fetch(BASE_URL)
+        .then(res => res.json())
+        .then(users => setTodos(users))
     }
 
-    console.log(tourneyWinner(competitions, results))
+    function createTodo() {
+        fetch(BASE_URL, {
+            method: 'POST',
+            body: JSON.stringify({
+                completed: true,
+                id: 201,
+                title: "new todos",
+                userId: 201
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        })
+        .then(res => res.json())
+        .then(json => console.log(json))
+    }
 
+    useEffect(() => {
+        fetchTodos()
+        createTodo()
+    }, [])
 
-
+    console.log(todos)
 
     return(
         <React.Fragment>
             <div className="app3-header">
-                <fieldset>
-                    <legend>Hi</legend>
-                </fieldset>
+                {/* {
+                    todos.map(todo => {
+                        return <div>
+                                    <h1>{todo.title}</h1>
+                                    <p>Completed: {todo.completed.toString()}</p>
+                                </div>
+                    })
+                } */}
             </div>
+            <button onClick={createTodo()}>Create</button>
         </React.Fragment>
     )
 
