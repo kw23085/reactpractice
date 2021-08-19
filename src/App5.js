3e35b4c6833810e4b29940085464a43539616744
Kevin Wang <kw1017@gmail.com>
2021-08-19 08:51:35 -0700

    light

---
+++ src/App5.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+
+function App5() {
+
+
+    const [pointer, setPointer] = useState(0)
+
+
+    const changeLight = () => {
+
+        if(pointer === 3) setPointer(0)
+    
+        setPointer(prv => prv += 1)
+
+    }
+
+
+    useEffect(() => {
+
+        setInterval(() => {
+
+            changeLight()
+    
+        }, 2000)
+
+    }, [])
+
+
+    return (
+        <React.Fragment>
+            <p>{pointer === 0 ? 'state is 0' : 'bye'}</p>
+            <p>{pointer === 1 ? 'state is 1' : 'bye'}</p>
+            <p>{pointer === 2 ? 'state is 2' : 'bye'}</p>
+        </React.Fragment>
+    )
+}
+
+export default App5
--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import App2 from './App2'
 import App3 from './App3'
 import App4 from './App4'
+import App5 from './App5'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App3 />
+    <App5 />
   </React.StrictMode>,
   document.getElementById('root')
 );
